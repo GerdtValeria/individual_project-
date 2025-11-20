99491b085a6279b56addfd867282511b8d00c866
GerdtValeria <gerdtvaleria33@gmail.com>
2025-11-20 16:32:59 +0300

    add_code

--- app/database/database.py
+++ app/database/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from app.config import settings
 
-engine = create_async_engine(settings.get_db_url)
+engine = create_async_engine(settings.db_url)
 
 async_session_maker = async_sessionmaker(bind=engine,
 expire_on_commit=False)
