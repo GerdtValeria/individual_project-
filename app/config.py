66bc17ba738df320a070c9bfef9885a98b0b8f0b
GerdtValeria <gerdtvaleria33@gmail.com>
2025-11-20 16:30:22 +0300

    add_code

--- app/config.py
+++ app/config.py
@@ -1,16 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DB_NAME: str
-    JWT_ALGORITHM: str
-    JWT_SECRET_KEY: str
-    
-    @property
-    def db_url(self):
+     SECRET_KEY: str
+     ALGORITHM: str
+     model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+    )  
+
+     @property
+     def db_url(self):
         return f"aiosqlite//{self.DB_NAME}"
 
-    @property
-    def auth_data(self):
+     @property
+     def auth_data(self):
         return {"algorithm": {self.JWT_ALGORITHM}, "secret_key": {self.JWT_SECRET_KEY}}
 
 settings = Settings()
