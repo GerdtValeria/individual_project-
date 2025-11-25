bad192a0aeeb083dbc76b1afd2114a12f3c96f13
GerdtValeria <gerdtvaleria33@gmail.com>
2025-11-25 19:11:50 +0300

    add_code

---
+++ app/api/comments.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/comments",tags=["Comment"])
+
+@router.get("/")
+async def get_comments():
+ comments = await CommentService().get_all_comments()   
+     return comments
+
+@router.get("/{id}")
+async def get_comment(id:int):
+    comment = await CommentService().get_all_comments(id=id)   
+     return comment
+
+@router.post("/")
+async def add_comment(comment_data: SCommentAdd):
+    await CommentService().add_comment(comment_data)
+
+@router.put("/{id}")
+async def edit_comment(id:int, comment_data: SCommentAdd):
+    data_comment = await CommentService().edit_comment(id,comment_data)
+    return data_comment
+
+@router.delete("/{id}")
+async def delete_comment(id:int):
+     await CommentService().delete_comment(id=id)   
---
+++ app/api/users.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/users",tags=["User"])
+
+@router.get("/")
+async def get_users():
+    users = await UserService().get_all_users()   
+     return users
+
+@router.get("/{id}")
+async def get_user(id:int):
+    user = await UserService().get_all_users(id=id)   
+     return user
+
+@router.post("/")
+async def add_user(user_data: SUserAdd):
+    await UserService().add_user(user_data)
+
+@router.put("/{id}")
+async def edit_user(id:int, user_data: SUserAdd):
+    data_user = await UserService().edit_user(id,user_data)
+    return data_user
+
+@router.delete("/{id}")
+async def delete_user(id:int):
+     await UserService().delete_user(id=id)   
