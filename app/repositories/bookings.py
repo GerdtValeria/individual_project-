9b9f7bc504906c59e0fe942a73d4d7acf6767779
GerdtValeria <gerdtvaleria33@gmail.com>
2025-11-28 10:20:39 +0300

    add_code

--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -4,7 +4,7 @@ from datetime import datetime, date
 from app.models.bookings import BookingsModel
 from app.repositories.base import BaseRepository
 
-class BookingRepository(BaseRepository):
+class BookingsRepository(BaseRepository):
     def __init__(self, session):
         super().__init__(session, BookingsModel)
 
